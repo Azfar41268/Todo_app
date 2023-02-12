d5812372147678e04f2f43f033c6a6556fca054f
Azfar41268 <azfarsikander33@gmail.com>
2023-02-12 09:54:27 +0500

    ok

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import AddNewTodo from "./add-new-todo"
+import List from "./list"
+
+export default function Home() {
+  return(
+    <div>
+      <AddNewTodo />
+      <List />
+    </div>
+  )
+}
